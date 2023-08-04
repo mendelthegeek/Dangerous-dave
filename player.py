f0abfb59c2672e2ec8b928a979b35f7b0b60236d
mendel keller <mendelthekeller@alum.mit.edu>
2023-08-04 12:37:54 -0400

    Wrapped sprite in class

--- player.py
+++ player.py
@@ -5,4 +5,4 @@ class Dave(pygame.sprite.Sprite):
 
     def __int__(self):
         self.position = (0, 0)
-        self.sprite_images = pygame.image.load(r"resources\dave\Dave.png")
+        self.sprite_source = r"resources\dave\Dave.png"
--- render.py
+++ render.py
@@ -8,14 +8,22 @@ board = pygame.display.set_mode((500, 500))
 def test_render(dave):
     board.fill(BG)
 
-    board.blit(get_dave_sprite(dave.sprite_images, 0, 24, 16, 2), dave.position)
-    board.blit(get_dave_sprite(dave.sprite_images, 3, 24, 16, 10), (250, 0))
+    sprite_sheet = SpriteSheet(dave.sprite_source)
+
+    board.blit(sprite_sheet.get_dave_sprite(0, 24, 16, 2), dave.position)
+    board.blit(sprite_sheet.get_dave_sprite(3, 24, 16, 10), (250, 0))
     pygame.display.flip()
 
 
-def get_dave_sprite(sprite_sheet, sheet, width, height, scale):
-    dave_sprite = pygame.Surface((width, height)).convert_alpha()
-    dave_sprite.blit(sprite_sheet, (0, 0), ((sheet * (width + 1)) + 1, 1, (1 + width * (sheet + 1)), height + 1))
-    dave_sprite = pygame.transform.scale(dave_sprite, (width * scale, height * scale))
-    dave_sprite.set_colorkey((0, 0, 0))
-    return dave_sprite
+class SpriteSheet:
+
+    def __init__(self, source):
+        self.sprite_sheet = pygame.image.load(source)
+
+    def get_dave_sprite(self, frame, width, height, scale):
+        dave_sprite = pygame.Surface((width, height)).convert_alpha()
+        sprite_rectangle = ((frame * (width + 1)) + 1, 1, (1 + width * (frame + 1)), height + 1)
+        dave_sprite.blit(self.sprite_sheet, (0, 0), sprite_rectangle)
+        dave_sprite = pygame.transform.scale(dave_sprite, (width * scale, height * scale))
+        dave_sprite.set_colorkey((0, 0, 0))
+        return dave_sprite
