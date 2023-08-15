847a5b408bf7eb9dfbbae2b74902df1ebf884902
mendel keller <mendelthekeller@alum.mit.edu>
2023-08-15 19:07:01 -0400

    Removed physics clutter

--- physics.py
+++ physics.py
@@ -6,19 +6,6 @@ import pygame
 arr = set()
 
 
-def test_new_jump_floor(dave, collided):
-    pass
-
-
-#             if dave.rect.bottom - 1 ==
-#             tile.top:
-#                 dave.on_surface = True
-#             elif dave.rect.top + 1 == tile.bottom:
-#                 dave.jump_height = 0
-#     overlap = max(dave.rect.right-tile.left, tile.right-dave.rect.left)
-#     if overlap > max_overlap:
-
-
 def check_collision(dave, tiles):
     # check for side,top or bottom collisions.
     # offset by one since collisions happen only after sprites overlap
@@ -51,35 +38,14 @@ def check_collision(dave, tiles):
             elif dave.rect.top + 1 == tile.bottom:
                 dave.jump_height = 0
 
-    # return
-
-    # # reset variable
-    # dave.on_surface = False
-    # check for bottom collision
     if dave.rect.bottom - 1 in [tile.top for tile in collided]:
         # check if we aren't confusing a side collision for a bottom collision
         if not side_collide:
             dave.on_surface = True
-        else:
-            test_new_jump_floor(dave, collided)
-            # # if we are in fact side colliding, things are a bit more difficult
-            # sorted_collisions = sorted(collided, key=operator.itemgetter(1))
-            # # we group the collided tiles by their y values and then
-            # for k, g in groupby(sorted_collisions, operator.itemgetter(1)):
-            #     # we check if there are two tiles on the same y values with different x values
-            #     if dave.rect.bottom - 1 == k and len(list(g)) > 1:
-            #         dave.on_surface = True
     # check if we bumped our head ouchie
     if dave.rect.top + 1 in [tile.bottom for tile in collided]:
         if not side_collide:
             dave.jump_height = 0
-        else:
-            test_new_jump_floor(dave, collided)
-            # # similar to above logic for bottom collision
-            # sorted_collisions = sorted(collided, key=operator.itemgetter(1))
-            # for k, g in groupby(sorted_collisions, operator.itemgetter(1)):
-            #     if dave.rect.top - 31 == k and len(list(g)) > 1:
-            #         dave.jump_height = 0
 
 
 def check_obtained(dave, obtainables):
